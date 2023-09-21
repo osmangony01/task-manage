846e007ae8b91cd68d6a713e993a3ff248e8a389
osmangony01 <mdosmangoni376@gmail.com>
2023-09-21 19:10:03 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import './App.css'
+import { Outlet } from 'react-router-dom'
+
+function App() {
+ 
+  return (
+    <div>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            hello I am home
+        </div>
+    );
+};
+
+export default Home;
