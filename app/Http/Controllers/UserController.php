7a1ab6ee873eb60409c7191962ae28bd8684fff8
osmangony01 <mdosmangoni376@gmail.com>
2024-02-12 14:58:11 +0600

    perform delete operation for user management

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class UserController extends Controller
 {
@@ -19,47 +20,70 @@ class UserController extends Controller
                     'status' => 200,
                     'users' => $users
                 ], 200);
-            } 
-            else {
+            } else {
                 return response()->json([
                     'status' => 404,
                     'users' => [],
                     'message' => 'No Records Found!!'
                 ], 404);
             }
-        } 
-        catch (ModelNotFoundException $exception) {
+        } catch (\Exception $exception) {
             return response()->json([
                 'status' => 500,
-                'message' => 'Something broke!!'
+                'message' => 'Something broke!!',
+                'exp' => $exception
             ], 500);
         }
     }
 
-    public function getUser($id){
-        try {
-            $users = User::findOrFail($id);
+    public function getUser($id)
+    {
 
-            if ($users->count() > 0) {
-                return response()->json([
-                    'status' => 200,
-                    'users' => $users
-                ], 200);
-            } 
-            else {
-                return response()->json([
-                    'status' => 404,
-                    'users' => [],
-                    'message' => 'No Records Found!!'
-                ], 404);
-            }
-        } 
-        catch (ModelNotFoundException $exception) {
+        try {
+            $user = User::findOrFail($id);
+    
+            return response()->json([
+                'status' => 200,
+                'user' => $user
+            ], 200);
+        } catch (ModelNotFoundException $exception) {
+            return response()->json([
+                'status' => 404,
+                'message' => 'User not found.'
+            ], 404);
+        } catch (\Exception $exception) {
+            // Log the detailed error for debugging
+            Log::error($exception);
+    
+            // Return a generic error response to the user
             return response()->json([
                 'status' => 500,
-                'message' => 'Something broke!!'
+                'message' => 'An error occurred while fetching the user.'
             ], 500);
         }
+        
+        // try {
+        //     $users = User::findOrFail($id);
+
+        //     if ($users->count() > 0) {
+        //         return response()->json([
+        //             'status' => 200,
+        //             'users' => $users
+        //         ], 200);
+        //     } else {
+        //         return response()->json([
+        //             'status' => 404,
+        //             'users' => [],
+        //             'message' => 'No Records Found!!'
+        //         ], 404);
+        //     }
+        // } catch (ModelNotFoundException $exception) {
+        //     // Log::error($exception);
+        //     return response()->json([
+        //         'status' => 500,
+        //         'message' => 'Something broke!!',
+        //     ], 500);
+        // }
     }
 
     public function addUser(Request $req)
@@ -90,8 +114,7 @@ class UserController extends Controller
                 'status' => 201,
                 'message' => 'User created successfully'
             ], 201);
-        } 
-        catch (\Exception $e) {
+        } catch (\Exception $e) {
             return response()->json([
                 'status' => 500,
                 'error' => 'Failed to create user, try again!'
@@ -121,14 +144,13 @@ class UserController extends Controller
             $user->email = $req->email;
             $user->password = $req->password;
             $user->bio = $req->bio;
-           
+
             if ($user->save()) {
                 return response()->json([
                     'status' => 202,
                     'message' => 'User updated successfully'
                 ], 202);
-            } 
-            else {
+            } else {
                 return response()->json([
                     'status' => 500,
                     'message' => 'Something went wrong, Update failed!'
@@ -136,13 +158,26 @@ class UserController extends Controller
             }
         } catch (\Exception $e) {
             return response()->json([
-                'status' => 500, 
+                'status' => 500,
                 'error' => 'Server error, failed to update!'
             ], 500);
         }
     }
 
-    public function deleteUser()
+    public function deleteUser($id)
     {
+        try {
+            $user = User::findOrFail($id);
+            $user->delete();
+            return response()->json([
+                'status' => 200,
+                'message' => 'User deleted successful',
+            ], 200);
+        } catch (ModelNotFoundException $exception) {
+            return response()->json([
+                'status' => 404,
+                'message' => 'User not found!',
+            ], 404);
+        }
     }
 }
